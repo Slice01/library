fb502205712b5ace9b7f4ab35c58956ae1986b77
Anthony Scott <eleventhhourtryst@gmail.com>
2023-03-28 16:04:17 -0500

    Removed old object

--- js/script.js
+++ js/script.js
@@ -14,14 +14,6 @@ class book {
     };
 };
 
-/*function book(title, author, pages, read) {
-    // the constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}*/
-
 function addBookToLibrary() {
     const newBookTitle = document.getElementById('titleField');
     const newBookAuthor = document.getElementById('authorField');
